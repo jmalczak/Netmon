9f1c3c20525658750ce706dc09e9158b10f9ec88
Jakub Malczak <jmalczak@gmail.com>
2014-04-15 15:04:30 +0200

    Update netmon.sh

--- netmon.sh
+++ netmon.sh
@@ -1,13 +1,28 @@
-ping www.google.com -c 100 > currentPing.txt
-packetLoss=`cat currentPing.txt | grep loss | awk -f netmon.awk`
-packetLossNumber=`echo ${packetLoss/%/}`
+pingCount=$1
+pingLossTreshold=$2
+pingDomain="www.google.com"
 
-if [ $packetLossNumber -lt 2 ]
+ping $pingDomain -c $pingCount > currentPing.txt
+
+numberOfPacketLossString=`cat currentPing.txt | grep loss | awk '{ print $7 }'`
+numberOfPacketLoss=`echo ${numberOfPacketLossString/%/}`
+
+if [ $numberOfPacketLoss -lt $pingLossTreshold ]
  then
         rm currentPing.txt
  else
-        currentDate=`date +"%m-%d-%Y_%H-%M"`
-        mv currentPing.txt "bad_$currentDate.txt"
+ 	# Try to send email with included log files
+        currentDate=`date +"%m-%d-%Y"`
+        currentHour=`date +"%H-%M"`
+        
+        if [! -d "Logs" ]; then
+  		mkdir Logs		
+	fi
+        
+        # Move file to Logs folder
+        mv currentPing.txt Logs/"PingLossAt_$currentDate_$curentHour.txt"
+	
+	# Generate email with attachments
 	python2.7 ../mail/mail_message_poorInternet.py "$currentDate"	
 fi
 
